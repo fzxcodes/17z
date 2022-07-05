5712f0ef6a919247c531c83b5e34bbe50ae31db3
FaizGanz <89491496+fzxcodes@users.noreply.github.com>
2022-07-05 07:55:26 +0700

    update hisoka.js

--- hisoka.js
+++ hisoka.js
@@ -546,7 +546,7 @@ Silahkan @${m.mentionedJid[0].split`@`[0]} untuk ketik terima/tolak`
             }
             break
             case 'sc': {
-                m.reply('Script : https://github.com/DikaArdnt/Hisoka-Morou\n\n Dont Forget Give Star\n\nDonate : 6281615075793 (Link Aja)\nSaweria : https://saweria.co/DikaArdnt\nPaypal : https://www.paypal.me/Cakhaho\n\n Dont Forget Donate')
+                m.reply('Script By DikaArdnt')
             }
             break
             case 'chat': {
