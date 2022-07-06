b0c31beaf2c54cf1324f2cc4ff6a288538e23ff0
FaizGanz <89491496+fzxcodes@users.noreply.github.com>
2022-07-06 09:47:25 +0700

    update hisoka.js

--- hisoka.js
+++ hisoka.js
@@ -1625,6 +1625,17 @@ break
                 hisoka.sendMessage(m.chat, { video: { url: media.dl_link }, mimetype: 'video/mp4', fileName: `${media.title}.mp4`, caption: `*Mohon Tunggu..*\n⭔ Title : ${media.title}\n⭔ File Size : ${media.filesizeF}\n⭔ Url : ${isUrl(text)}\n⭔ Ext : MP3\n⭔ Resolusi : ${args[1] || '360p'}` }, { quoted: m })
             }
             break
+            case 'linkgcalumni': {
+            	kar = `Halo ${pushname}.\n
+            Link GC Alumni Ver. Siswa (TANPA GURU) :
+            https://chat.whatsapp.com/JsCdG1rMztXJC3A5SU4L6j\n
+            Link GC Ngagebrot 17Z :
+            https://chat.whatsapp.com/IHOXYWgD560CI1INaTqPBG`
+            m.reply(kar)
+            }
+            break
+            
+         
 	    case 'getmusic': {
                 let { yta } = require('./lib/y2mate')
                 if (!text) throw `Example : ${prefix + command} 1`
